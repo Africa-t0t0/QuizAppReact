617b481327fd8aed5edfb7a15d674560aba8cc59
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2024-12-04 23:08:47 -0300

    functional basic version

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,45 @@
 import { useState } from "react";
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
 
+    function handleSelectedAnswer(selectedAnswer) {
+
+        setUserAnswers((prevUserAnswer) => {
+            return [...prevUserAnswer, selectedAnswer];
+        });
+    }
+
     return (
-        <>
-            <p>Currently active question</p>
-        </>
+        <div
+            id="quiz"
+        >
+            <div id="question">
+                <p>Currently active question</p>
+                <h2>
+                    {QUESTIONS[activeQuestionIndex].text}
+                    <ul
+                        id="answers"
+                    >
+                        {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                            <li
+                                className="answer"
+                                key={answer}
+                            >
+                                <button
+                                    onClick={() => handleSelectedAnswer(answer)}
+                                >
+                                    {answer}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </h2>
+            </div>
+        </div>
     );
 }

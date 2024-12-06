5cb8f5b8c2c57cf1bbb867b183e7ab9f1f7a9c4e
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2024-12-05 21:54:16 -0300

    add functional progress bar

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,31 +1,64 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import QUESTIONS from '../questions.js';
+
+import quizCompletedImg from '../assets/quiz-complete.png';
+
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
 
-    function handleSelectedAnswer(selectedAnswer) {
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-        setUserAnswers((prevUserAnswer) => {
-            return [...prevUserAnswer, selectedAnswer];
-        });
+    const handleSelectedAnswer =  useCallback(
+        function handleSelectedAnswer(selectedAnswer) {
+
+            setUserAnswers((prevUserAnswer) => {
+                return [...prevUserAnswer, selectedAnswer];
+            });
+        }, []
+    );
+
+    const handleSkipAnswer = useCallback(() => handleSelectedAnswer(null)
+    , [handleSelectedAnswer])
+
+    if (quizIsComplete) {
+        return (
+            <>
+                <div id="summary">
+                    <img
+                        src={quizCompletedImg}
+                        alt="Trophy Icon"
+                    />
+                    <h2>
+                        Quiz Completed!
+                    </h2>
+                </div>
+            </>
+        );
     }
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     return (
         <div
             id="quiz"
         >
             <div id="question">
+                <QuestionTimer
+                    timeout={10000}
+                    onTimeout={null}
+                />
                 <p>Currently active question</p>
                 <h2>
                     {QUESTIONS[activeQuestionIndex].text}
                     <ul
                         id="answers"
                     >
-                        {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                        {shuffledAnswers.map(answer => (
                             <li
                                 className="answer"
                                 key={answer}
